468fab7a34ee3c0b8fe2ce5a30e7fb59662769e2
Nyam-bura <nyamburaajanee@gmail.com>
2022-03-10 08:48:15 +0300

    first commit

---
+++ main.kt
@@ -0,0 +1,30 @@
+fun main (){
+
+}
+fun school(){
+    var institution = "codeHive"
+    println(institution[4]+institution[5].toString()+institution[6]+institution[7])
+}
+//Create and invoke a function that given two numbers returns the
+remainder of their division
+ fun modulus {
+     var division =(Int:y,Int:x)
+     println(division)
+
+ }
+//write a function that takes in two parameters, name and age and returns a
+String with this structure “Hi, my name is p and I am z years old.” Where p
+and  z are your  name and age respectively.
+        fun main(){
+            var name ="Jane"
+            var age = 20
+            var statement ="Hi,my name is $name and am $age"
+            println(statement)
+
+        }
+//Write a function that takes in a string and returns its length
+fun main(){
+    var word ="Fruitful"
+    println(word.length)
+}
+
